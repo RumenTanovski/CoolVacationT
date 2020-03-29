bcc933d915553c1b04e6de1b2de00a94143b6ce4
RumenTanovski <rwttroyan@yahoo.com>
2020-03-29 10:28:46 +0300

    Add Required to FeedBack

--- Data/CoolVacationT.Data.Models/FeedBack.cs
+++ Data/CoolVacationT.Data.Models/FeedBack.cs
@@ -6,8 +6,12 @@
 
     public class FeedBack : BaseDeletableModel<string>
     {
+        [Required]
+        [Range(1, 10)]
         public int Rating { get; set; }
 
+        [Required]
+        [StringLength(250, MinimumLength = 3)]
         public string Comment { get; set; }
 
         [Required]
---
+++ Data/CoolVacationT.Data/Migrations/20200329071856_FeedBackRequired.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoolVacationT.Data.Migrations
+{
+    public partial class FeedBackRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "FeedBacks",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "FeedBacks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 250);
+        }
+    }
+}
