09dd6d1e23634df1a6f680d6e17c640b986fe743
RumenTanovski <rwttroyan@yahoo.com>
2020-04-01 15:51:35 +0300

    Add Carusel, fontawesome, Page Introduction

--- CoolVacationT.Common/GlobalConstants.cs
+++ CoolVacationT.Common/GlobalConstants.cs
@@ -5,5 +5,18 @@
         public const string SystemName = "CoolVacationT";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string PicHomeOut = "https://apriltsi.org/uploads/hotels-images/321/DSC_0229.jpg";
+
+        public const string PicHol = "https://apriltsi.org/uploads/hotels-images/483/velena_hol.jpg";
+
+        public const string PicTrapezaria = "https://apriltsi.org/uploads/hotels-images/482/velena_trapezariya.jpg";
+
+        public const string PicKitchen = "https://https://apriltsi.org/uploads/hotels-images/487/251876_439461772742230_1327222704_n.jpg";
+
+        public const string PicCoupleRoom = "https://apriltsi.org/uploads/hotels-images/484/velena_dvoina_staq.jpg";
+
+        public const string Describe = "Тел.: + 359 888 625 021, +359 887 449 714";
+
     }
 }
