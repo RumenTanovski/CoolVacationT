b6b881c3ddd25d1cc247c4631f4d64014b65c185
RumenTanovski <rwttroyan@yahoo.com>
2020-04-08 12:58:47 +0300

    Update FeedBack input form

--- Web/CoolVacationT.Web/Controllers/FeedBacksController.cs
+++ Web/CoolVacationT.Web/Controllers/FeedBacksController.cs
@@ -1,8 +1,5 @@
 ﻿namespace CoolVacationT.Web.Controllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
 
     using CoolVacationT.Data.Models;
