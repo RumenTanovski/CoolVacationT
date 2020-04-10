cc79058340433443ccbddd556991e31bbd93b5b2
RumenTanovski <rwttroyan@yahoo.com>
2020-04-10 10:34:13 +0300

    Modified GlobalConstants

--- CoolVacationT.Common/GlobalConstants.cs
+++ CoolVacationT.Common/GlobalConstants.cs
@@ -6,15 +6,15 @@
 
         public const string AdministratorRoleName = "Administrator";
 
-        public const string PicHomeOut = "https://apriltsi.org/uploads/hotels-images/321/DSC_0229.jpg";
+        public const string PicHomeOut = "http://res.cloudinary.com/du8vkjq9e/image/upload/v1586502384/ath0ggbmtuamgujtr42t.jpg";
 
-        public const string PicHol = "https://apriltsi.org/uploads/hotels-images/483/velena_hol.jpg";
+        public const string PicHol = "http://res.cloudinary.com/du8vkjq9e/image/upload/v1586502667/xgoj5g7isi1wyitjlbxg.jpg";
 
-        public const string PicTrapezaria = "https://apriltsi.org/uploads/hotels-images/482/velena_trapezariya.jpg";
+        public const string PicTrapezaria = "http://res.cloudinary.com/du8vkjq9e/image/upload/v1586502762/fjbhwx1fh8msexjeaduc.jpg";
 
-        public const string PicKitchen = "https://apriltsi.org/uploads/hotels-images/487/251876_439461772742230_1327222704_n.jpg";
+        public const string PicKitchen = "http://res.cloudinary.com/du8vkjq9e/image/upload/v1586502845/gfltkn4nw6nmqo5nng1s.jpg";
 
-        public const string PicCoupleRoom = "https://apriltsi.org/uploads/hotels-images/484/velena_dvoina_staq.jpg";
+        public const string PicCoupleRoom = "http://res.cloudinary.com/du8vkjq9e/image/upload/v1586502927/dihelhu9xk6tvgtpjjnh.jpg";
 
         public const string Describe = "Тел.: + 359 888 625 021, +359 887 449 714";
 
