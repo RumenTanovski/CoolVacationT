f1f6ac1fd2af32d651e15909b694449d2da917f3
RumenTanovski <rwttroyan@yahoo.com>
2020-04-19 20:31:13 +0300

    Upgrade PaymentServiceTests

--- Tests/CoolVacationT.Services.Data.Tests/PaymentServiceTests.cs
+++ Tests/CoolVacationT.Services.Data.Tests/PaymentServiceTests.cs
@@ -30,5 +30,24 @@
             Assert.Equal("Plategno 125/15.04.2020", list[0].DocumentNumber);
             Assert.Equal("http://res.cloudinary.com/...", list[0].StringFileCloud);
         }
+
+        [Fact]
+        public void AddPaymentTwoArgumentCorrectly()
+        {
+            var dbContext = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var repository = new EfDeletableEntityRepository<Payment>(new ApplicationDbContext(dbContext.Options));
+
+            repository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            var service = new PaymentService(repository);
+
+            _ = service.AddAsync(1000, "Plategno 125/15.04.2020");
+            var list = repository.All().ToList();
+
+            Assert.Single(list);
+            Assert.Equal(1000, list[0].AmountPaid);
+            Assert.Equal("Plategno 125/15.04.2020", list[0].DocumentNumber);
+        }
     }
 }
